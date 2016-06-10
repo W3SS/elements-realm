2934f9b2ab71d8c6cf4cb22cbb5520980c09ba65
adrpadua <adrpadua@gmail.com>
2016-06-09 23:44:24 -0700

    Menu is prettier

---
+++ coredata-practice/MenuButton.swift
@@ -0,0 +1,26 @@
+//
+//  MenuButton.swift
+//  coredata-practice
+//
+//  Created by Adrian Padua on 6/9/16.
+//  Copyright © 2016 Adrian Padua. All rights reserved.
+//
+
+import UIKit
+
+class MenuButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+        clipsToBounds = true
+    }
+
+}
